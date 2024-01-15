e630505e4342dc0e1dc2c6741544dee6bc1fc644
yd <k.yoonduk@gmail.com>
2024-01-15 17:49:57 -0500

    Add metadata generation and data retrieval for blog post layout

--- app/(subpages)/blog/[slug]/layout.tsx
+++ app/(subpages)/blog/[slug]/layout.tsx
@@ -1,9 +1,90 @@
 import BlogPostFooter from "@/_components/footer";
+import getPosts from "@/lib/get-posts";
+import { Metadata } from "next";
 
-const BlogPostLayout = ({ children }: { children: React.ReactNode }) => {
+export const generateMetadata = async ({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}): Promise<Metadata> => {
+  const post = (await getPosts()).find((p) => p?.slug === params.slug);
+  return {
+    // metadataBase: new URL("http://localhost:3000"),
+    title: post?.title,
+    description: post?.description,
+    alternates: {
+      canonical: `https://kimyoonduk.com/blog/${params.slug}`,
+    },
+  };
+};
+
+async function getData({ slug }: { slug: string }) {
+  const posts = await getPosts();
+  const postIndex = posts.findIndex((p) => p?.slug === slug);
+
+  if (postIndex === -1) {
+    throw new Error(
+      `${slug} not found in posts. Did you forget to rename the file?`
+    );
+  }
+
+  const post = posts[postIndex];
+
+  const { ...rest } = post;
+
+  return {
+    previous: posts[postIndex + 1] || null,
+    next: posts[postIndex - 1] || null,
+    ...rest,
+  };
+}
+
+const BlogPostLayout = async ({
+  children,
+  params,
+}: {
+  children: JSX.Element;
+  params: {
+    slug: string;
+  };
+}) => {
+  const { previous, next, title, date, lastModified } = await getData(params);
+
+  // currently not used. Update getPosts to check commit history for last modified date.
+  const lastModifiedDate = lastModified
+    ? new Date(lastModified).toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      })
+    : null;
+
+  const formattedDate = date
+    ? new Date(date).toLocaleDateString("en-US", {
+        month: "long",
+        day: "numeric",
+        year: "numeric",
+      })
+    : null;
+
   return (
     <div className="prose prose-sm md:prose-base w-full max-w-4xl flex-grow">
-      <div className="px-4">{children}</div>
+      <div className="px-4">
+        <div className="mb-4">
+          <span className="">{formattedDate}</span>
+          <div className="text-xs">
+            {lastModified ? (
+              <span className="">Last modified {lastModifiedDate}</span>
+            ) : null}
+          </div>
+        </div>
+        <article>
+          <h1>{title}</h1>
+          {children}
+        </article>
+      </div>
       <BlogPostFooter />
     </div>
   );
